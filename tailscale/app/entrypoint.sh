4d99d1a83377d9aa0872ac52fd0e4ec147a5e158
Daniel Hall <hassio@danielhall.me>
2021-06-13 14:11:54 +1000

    Turn on forwarding

--- tailscale/app/entrypoint.sh
+++ tailscale/app/entrypoint.sh
@@ -2,6 +2,8 @@
 set -m # enables job control to use fg command
 kubectl create configmap $TAILSCALE_CONFIG_MAP # create the config map to backup tailscale data if it doesn't exist
 
+sysctl -w net.ipv4.ip_forward=1
+
 tailscaled >/dev/null 2>&1 & # start the tailscale daemon
 sleep 10 # allows for the service to boot an connect before trying to register container with tailscale
 tailscale up -hostname $TAILSCALE_HOSTNAME -authkey $TAILSCALE_AUTH_KEY -advertise-routes $TAILSCALE_ADVERTISE_ROUTES # connect to VPN
